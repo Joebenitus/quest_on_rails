ca62bca10d425c2ceaee2bcc6deeda9a7b13d0f3
Joseph Karnafel <josephkarnafel@gmail.com>
2020-10-20 14:05:53 -0700

    add passing tests for player object having many items and quests Co-authored-by: AJ Markow <alexanderjmarkow@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
- belongs_to :location
+          :recoverable, :rememberable, :validatable
+  belongs_to :location
+  has_many :items
+  has_many :quests
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 describe User do
   it {should belong_to (:location)}
+  it {should have_many (:items)}
+  it {should have_many (:quests)}
 end
